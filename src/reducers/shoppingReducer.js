e44023d82f4b870aa435b358b9abb52de0da0d10
daphnalex <daphne.bordel@gmail.com>
2019-08-24 19:07:21 +0200

    bug when add several articles

--- src/reducers/shoppingReducer.js
+++ src/reducers/shoppingReducer.js
@@ -7,10 +7,11 @@ const initalState = {
 export const shoppingReducer = (state = initalState, action) => {
     //console.log('enter in shoppingReducer',state);
     //console.log('action.type',action.type);
+    console.log("length",state.articles.length);
     switch(action.type){
         case ADD_ARTICLE:
-            console.log("entre dans ADD",state)
-            //first case : articles is empty
+            console.log("entre dans ADD",state);
+            
             if(state.articles.length === 0){
                 action.payload.quantity = 1;
                 const newState = {
@@ -19,36 +20,44 @@ export const shoppingReducer = (state = initalState, action) => {
                 }
                 console.log("state afer 1",newState);
                 return newState;
-            } else {
-                //second case : array articles is not empty - we map the array
-                const newArticles = state.articles.map(article => {
+            };
+
+            if(state.articles.length > 0){
+                let testIsNewArticle = true;
+                const newArticles = state.articles.map((article,i) => {
                     if (article.reference === action.payload.reference){
                         //if reference of article exist we add one quantity of the article
-                        article.quantity ++;
+                        testIsNewArticle = false;
                         console.log("article avant de le retourner",article);
-                        return article;
-                    } else {
-                        //if reference of article not exist we add the new article
-                        console.log("action.payload",action.payload);
-                        const newState = [
+                       action.payload.quantity = article.quantity + 1;
+                        console.log("state avant retour",state.articles);
+                        const newArticle = {
+                            ...article,
+                            ...action.payload
+                        }
+                        console.log("newArticle",newArticle);
+                        return newArticle;
+                    }
+                   return state;
+                });
+                 if (testIsNewArticle === true){
+                     console.log("c'est un nouvel article");
+                    //if article not exist we add it to the array
+                    action.payload.quantity = 1;
+                    return {
+                        ...state,
+                        articles: [
                             ...state.articles,
                             action.payload
                         ]
-                        return newState;
                     }
-                });
-                console.log("newArticles",newArticles);
-                console.log('state after 2',state.articles);
-                //return newArticles;
-                const test = {
-                    ...state,
-                    articles: newArticles
-                };
-                console.log("test",typeof(test.articles));
-                return test;
-            }
-            
-           
+                } else {
+                   console.log("on verra",newArticles);
+                   console.log('newState',state);
+                   return {...state, articles : newArticles};
+                }
+                return state;
+            };
         default:
             return state;
     }
